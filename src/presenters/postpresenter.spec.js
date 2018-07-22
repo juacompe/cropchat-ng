e09cfea9eb9d1833d4a114a0a05f69e8557767a4
juacompe <juacompe@gmail.com>
2018-07-22 19:35:06 +0700

    Add PostPresenter and CatAPI

---
+++ src/presenters/postpresenter.spec.js
@@ -0,0 +1,68 @@
+import PostPresenter from './PostPresenter'
+import flushPromises from 'flush-promises'
+describe('PostPresenter', () => {
+  let presenter
+  beforeEach(() => {
+    presenter = new PostPresenter()
+  })
+  describe('constructor', () => {
+    it('should have catUrl or the image would not display on post page', () => {
+      expect(presenter.catUrl).toEqual('')
+    })
+  })
+  describe('postCat', () => {
+    it('should have url', () => {
+      let view = input('catUrl', 'url')
+      presenter.postCat()
+      view.validate()
+    })
+
+    it('should have comment', () => {
+      let view = input('title', 'comment')
+      presenter.postCat()
+      view.validate()
+      flushPromises()
+    })
+
+    describe('success', () => {
+      let view
+      it('should go back to home page', async () => {
+        view = new MockBackToHome()
+        presenter.view = view
+        presenter.catUrl = 'url'
+        presenter.title = 'title'
+        presenter.postCat()
+        await flushPromises()
+        view.validate()
+      })
+
+      class MockBackToHome {
+        constructor () { this.called = false }
+        pushNewCat () { return Promise.resolve() }
+        backToHome () { view.called = true }
+        validate () {
+          expect(this.called).toEqual(true)
+        }
+      }
+    })
+    function input (textField, jsonField) {
+      let view = new StubPushNewCat(jsonField)
+      presenter[textField] = view.expected
+      presenter.view = view
+      return view
+    }
+    class StubPushNewCat {
+      constructor (field) {
+        this.expected = 'expected'
+        this.field = field
+      }
+      pushNewCat (newCat) {
+        this.actual = newCat[this.field]
+        return Promise.resolve()
+      }
+      validate () {
+        expect(this.expected).toEqual(this.actual)
+      }
+    }
+  })
+})
