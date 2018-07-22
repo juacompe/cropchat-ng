45fc1e97e996d97ac53b70431d63115302e899e1
juacompe <juacompe@gmail.com>
2018-07-22 19:15:57 +0700

    Add menu
    using https://github.com/mseemann/angular2-mdl
    ref: http://mseemann.io/angular2-mdl/layout

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should have CropChat title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to cropchat-ng!');
+    expect(page.getTitle()).toEqual('CropChat');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdlModule } from '@angular-mdl/core'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
